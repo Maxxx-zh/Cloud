bb36fc9052aea721082878a376c33f5eba8f7793
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2022-12-05 18:25:41 +0200

    Final Fix

--- hometask7/main.tf
+++ hometask7/main.tf
@@ -1,6 +1,8 @@
 # Create a custom VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
 }
 
 # Create Internet Gateway
@@ -21,14 +23,14 @@ resource "aws_route_table" "my_route_table" {
 # Create Subnets 
 resource "aws_subnet" "my_subnet1" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.10.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "my_subnet2" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.10.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1b"
 }
@@ -43,27 +45,12 @@ resource "aws_route_table_association" "table_association2" {
   route_table_id = aws_route_table.my_route_table.id
 }
 
-
 # Create Security Group to allow port 80
 resource "aws_security_group" "allow_web" {
   name        = "allow_web_traffic"
   description = "Allow Web inbound traffic"
   vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -92,11 +79,11 @@ resource "aws_network_acl" "bar" {
 resource "aws_network_acl_rule" "bar" {
   network_acl_id = aws_network_acl.bar.id
   rule_number    = 100
-  protocol       = "tcp"
+  protocol       = -1
   rule_action    = "deny"
   cidr_block     = "50.31.252.0/24"
-  from_port      = 80
-  to_port        = 80
+  from_port      = 0
+  to_port        = 0
 }
 
 # Add DB subnet group
@@ -110,7 +97,7 @@ variable "MYSQL_PWD" {}
 
 # Create RDS Instance
 resource "aws_db_instance" "my_instance" {
-  allocated_storage      = 10
+  identifier             = "mysqldb"
   db_name                = "dbtest"
   engine                 = "mysql"
   engine_version         = "5.7"
@@ -118,8 +105,11 @@ resource "aws_db_instance" "my_instance" {
   username               = "testuser"
   password               = var.MYSQL_PWD
   port                   = "3306"
+  storage_type           = "gp2"
+  allocated_storage      = 20
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   db_subnet_group_name   = aws_db_subnet_group.db_sg.id
   parameter_group_name   = "default.mysql5.7"
   skip_final_snapshot    = true
+  publicly_accessible    = true
 } 
